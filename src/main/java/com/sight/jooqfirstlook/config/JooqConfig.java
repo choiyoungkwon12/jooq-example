c6b1e5b0d03febfe83930e9af97b41f5da5eeffc
choiyoungkwon12 <hpwh4457@naver.com>
2024-07-21 05:33:20 +0900

    slow 쿼리 감지하는 execute listener 구현

--- src/main/java/com/sight/jooqfirstlook/config/JooqConfig.java
+++ src/main/java/com/sight/jooqfirstlook/config/JooqConfig.java
@@ -9,9 +9,13 @@ import org.springframework.context.annotation.Configuration;
 public class JooqConfig {
     @Bean
     public DefaultConfigurationCustomizer jooqDefaultConfigurationCustomizer() {
-        return configuration -> configuration.settings()
-                .withExecuteDeleteWithoutWhere(ExecuteWithoutWhere.THROW)
-                .withExecuteUpdateWithoutWhere(ExecuteWithoutWhere.THROW)
-                .withRenderSchema(false);
+        return configuration -> {
+            // execute listener 등록
+            configuration.set(PerformanceListener::new);
+            configuration.settings()
+                    .withExecuteDeleteWithoutWhere(ExecuteWithoutWhere.THROW)
+                    .withExecuteUpdateWithoutWhere(ExecuteWithoutWhere.THROW)
+                    .withRenderSchema(false);
+        };
     }
 }
